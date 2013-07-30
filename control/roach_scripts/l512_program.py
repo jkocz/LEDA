e1b3b8da18580f434da816ff9ebd1593b8ddcec9
jkocz <jkocz@github.com>
2013-07-30 11:11:28 -0700

    programming scripts

---
+++ control/roach_scripts/l512_program.py
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+adc16_init.rb rofl1 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl2 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl3 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl4 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl5 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl6 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl7 l512_t2_fnew_z2_tgbe3and4_2013_Jul_29_2217.bof
+adc16_init.rb rofl8 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl9 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl10 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl11 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl12 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl13 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl14 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl15 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+adc16_init.rb rofl16 l512_t2_fnew_z2_2013_Jul_20_1343.bof
+
