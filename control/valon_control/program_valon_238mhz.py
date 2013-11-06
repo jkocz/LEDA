371e183d2e2691dd0a26ac083e1fb9ac73a0cf51
Danny Price <dan@thetelegraphic.com>
2013-11-06 16:13:53 -0500

    Adding Valon setup scripts for specific frequencies
    Signed-off-by: Danny Price <dan@thetelegraphic.com>

---
+++ control/valon_control/program_valon_238mhz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from valon_synth import *
+
+if __name__ == "__main__":
+    synth = Synthesizer("/dev/ttyUSB0")
+    print "Freq:    ", synth.get_frequency(SYNTH_A)
+    print "Ref:     ", "external" if synth.get_ref_select()==1 else "internal"
+    print "Ref freq:", synth.get_reference()
+    
+    synth.set_frequency(SYNTH_A, 238.0, .01)
+    print "Freq:    ", synth.get_frequency(SYNTH_A)
+    synth.flash() 
+     
+    #ref = "external"
+    #ref = "internal"
+    #synth.set_ref_select(1 if ref=="external" else 0)
+    #synth.flash()
